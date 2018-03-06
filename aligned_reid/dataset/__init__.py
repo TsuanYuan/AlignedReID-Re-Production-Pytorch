e20d37a59569d0be57ef0bc1d5bea6c389d0c22a
Quan Yuan <qyuan@aibee.com>
2018-03-06 15:41:59 -0500

    public3 in train-test

--- aligned_reid/dataset/__init__.py
+++ aligned_reid/dataset/__init__.py
@@ -13,7 +13,7 @@ def create_dataset(
     name='market1501',
     part='trainval',
     **kwargs):
-  assert name in ['market1501', 'cuhk03', 'duke', 'folder', 'folder_train_test','combined'], \
+  assert name in ['market1501', 'cuhk03', 'duke', 'public3','folder', 'folder_train_test','combined'], \
     "Unsupported Dataset {}".format(name)
 
   assert part in ['trainval', 'train', 'val', 'test'], \
