c75fcdd1c4ca1818b5b0346cb51d553d3a1d435c
Quan Yuan <qyuan@aibee.com>
2018-05-18 09:28:23 +0800

    fix diag type

--- set2set/losses.py
+++ set2set/losses.py
@@ -86,11 +86,13 @@ def weighted_seq_loss_func(feature, weight, pids, seq_size, margin):
     pid_expand = pids.expand(feature_size[0], num_feature_per_id).contiguous().view(-1)  # unfold to [n*m]
 
     N = pid_expand.size()[0]  # number of weighted features
-    diag_one = torch.eye(N).type(torch.ByteTensor)
-    if torch.has_cudnn:
-        diag_one.cuda()
-
-    is_pos = pid_expand.expand(N, N).eq(pid_expand.expand(N, N).t()) - diag_one
+    #diag_one = torch.eye(N).type(torch.ByteTensor)
+    #if torch.has_cudnn:
+    #    diag_one.cuda()
+    #torch.diag(torch.ones_like())
+    is_pos = pid_expand.expand(N, N).eq(pid_expand.expand(N, N).t()) 
+    diag_one = torch.diag(torch.ones_like(torch.sum(is_pos,1)))
+    is_pos = is_pos - diag_one
     is_neg = pid_expand.expand(N, N).ne(pid_expand.expand(N, N).t())
     dist_mat = euclidean_distances(summed_feature_normalize)
 
