d6d539413fd6bc8d1e3b68d0c88bd363f629efb1
Quan Yuan <yqsony@gmail.com>
2018-07-14 23:41:22 -0700

    sample local id

--- aligned_reid/dataset/TrainSet.py
+++ aligned_reid/dataset/TrainSet.py
@@ -54,12 +54,12 @@ class TrainSet(Dataset):
 
   def get_sample_within_interval(self, im_inds):
     im_names_valid = []
-    start_ind = np.random.choice(im_inds, 1)[0]
-    max_ind = min(len(im_inds), start_ind+self.frame_interval)
-    _ ,start_cid, start_fid = self.decode_im_file_name(self.im_names[start_ind])
+    start_ind_local = np.random.choice(len(im_inds), 1)[0]
+    max_ind_local = min(len(im_inds), start_ind_local+self.frame_interval)
+    _ ,start_cid, start_fid = self.decode_im_file_name(self.im_names[im_inds[start_ind_local]])
     # get all valid im names within a time interval
-    for i in range(start_ind, max_ind):
-      im_name = osp.basename(self.im_names[i])
+    for i in range(start_ind_local, max_ind_local):
+      im_name = osp.basename(self.im_names[im_inds[i]])
       _, camera_id, frame_index = self.decode_im_file_name(im_name)
       if camera_id != start_cid or abs(frame_index-start_fid)>self.frame_interval:
         break
