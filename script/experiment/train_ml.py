a7ace0f9d635c8055496aa854a66f15a2f1b457c
Quan Yuan <qyuan@aibee.com>
2018-08-16 01:59:51 +0800

    upper backbone resnet34

--- script/experiment/train_ml.py
+++ script/experiment/train_ml.py
@@ -652,7 +652,7 @@ def main():
         g_dist_an_meter.update(g_d_an)
         g_loss_meter.update(to_scalar(g_loss))
 
-        if cfg.l_loss_weight > 0 and (not cfg.parts_model) and (not cfg.use_MGN) and (not cfg.use_attn):
+        if cfg.l_loss_weight > 0 and (not cfg.parts_model) and len(cfg.model_name)==0: #not cfg.parts_model) and (not cfg.use_MGN) and (not cfg.use_attn):
           # precision
           l_prec = (l_dist_an > l_dist_ap).data.float().mean()
           # the proportion of triplets that satisfy margin
