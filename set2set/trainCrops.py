d57fa40592ad3c242e9c2f3cbf9fee4a00332ae8
Quan Yuan <yqsony@gmail.com>
2018-09-09 15:29:43 -0700

    resume options

--- set2set/trainCrops.py
+++ set2set/trainCrops.py
@@ -8,13 +8,43 @@ import torch.backends.cudnn
 from DataLoader import ReIDAppearanceSet2SetDataset
 import argparse
 import os
+import datetime
 
 from torchvision import transforms
 import transforms_reid, Model
-import utils
 import losses
 from torch.autograd import Variable
-import debug_tool
+from torch.nn.parallel import DataParallel
+
+
+def load_ckpt(modules_optims, ckpt_file, load_to_cpu=True, verbose=True, skip_fc=False):
+  """Load state_dict's of modules/optimizers from file.
+  Args:
+    modules_optims: A list, which members are either torch.nn.optimizer
+      or torch.nn.Module.
+    ckpt_file: The file path.
+    load_to_cpu: Boolean. Whether to transform tensors in modules/optimizers
+      to cpu type.
+  """
+  map_location = (lambda storage, loc: storage) if load_to_cpu else None
+  ckpt = torch.load(ckpt_file, map_location=map_location)
+  if skip_fc:
+    print('skip fc layers when loading the model!')
+  for m, sd in zip(modules_optims, ckpt['state_dicts']):
+    if m is not None:
+      if skip_fc:
+        for k in sd.keys():
+          if k.find('fc') >= 0:
+            sd.pop(k, None)
+      if hasattr(m, 'param_groups'):
+        m.load_state_dict(sd)
+      else:
+        m.load_state_dict(sd, strict=False)
+  if verbose:
+    print('Resume from ckpt {}, \nepoch {}, \nscores {}'.format(
+      ckpt_file, ckpt['ep'], ckpt['scores']))
+  return ckpt['ep'], ckpt['scores']
+
 
 def adjust_lr_staircase(optimizer, base_lr, ep, decay_at_epochs, factor):
     """Multiplied by a factor at the BEGINNING of specified epochs. All
@@ -62,8 +92,8 @@ def init_optim(optim, params, lr, weight_decay, eps=1e-8):
         raise KeyError("Unsupported optim: {}".format(optim))
 
 def main(data_folder, model_folder, sample_size, batch_size,
-         num_epochs=200, gpu_id=-1, margin=0.1, base_model='resnet18', loss_name='ranking',
-         optimizer_name='adam', base_lr=0.001, weight_decay=5e-04, threshold=0.1, original_ar=False, with_roi=False):
+         num_epochs=200, gpu_ids=None, margin=0.1, base_model='resnet18', loss_name='ranking',
+         optimizer_name='adam', base_lr=0.001, weight_decay=5e-04, threshold=0.1, with_roi=False):
     if with_roi:
         composed_transforms = transforms.Compose([transforms_reid.RandomHorizontalFlip(),
                                                   transforms_reid.Rescale((256, 128)),
@@ -85,12 +115,13 @@ def main(data_folder, model_folder, sample_size, batch_size,
                             shuffle=True, num_workers=4)
 
     if not torch.cuda.is_available():
-        gpu_id = -1
+        gpu_ids = None
 
-    if gpu_id>=0:
-        model = Model.MGNModel().cuda(device=gpu_id)
-    else:
-        model = Model.WeightedReIDFeatureModel(base_model=base_model,num_classes=num_classes)
+    #if len(gpu_ids)>=0:
+    model = Model.MGNModel()
+    # else:
+    #     model = Model.WeightedReIDFeatureModel(base_model=base_model,num_classes=num_classes)
+    optimizer = init_optim(optimizer_name, model.parameters(), lr=base_lr, weight_decay=weight_decay)
 
     if not os.path.isdir(model_folder):
         os.makedirs(model_folder)
@@ -98,7 +129,12 @@ def main(data_folder, model_folder, sample_size, batch_size,
         print('model folder {0} already exist, will overwrite it.'.format(model_folder))
     model_file = os.path.join(model_folder, 'model.ckpt')
     print('model path is {0}'.format(model_file))
-
+    if args.resume and os.path.isfile(model_file):
+        load_ckpt([model], model_file)
+    if len(gpu_ids) > 0:
+        model_p = DataParallel(model, device_ids=gpu_ids)
+    else:
+        model_p = model
     decay_at_epochs = {80:1, 120:2}
     staircase_decay_multiply_factor = 0.1
     if loss_name == 'ranking':
@@ -107,9 +143,6 @@ def main(data_folder, model_folder, sample_size, batch_size,
         loss_function = losses.WeightedAverageThLoss(th=threshold)
     else:
         raise Exception('unknown loss name')
-
-    optimizer = init_optim(optimizer_name, model.parameters(), lr=base_lr, weight_decay=weight_decay)
-    #average_meter = utils.AverageMeter()
 
     for epoch in range(num_epochs):
         sum_loss = 0
@@ -122,13 +155,13 @@ def main(data_folder, model_folder, sample_size, batch_size,
             images_5d = sample_batched['images']  # [batch_id, crop_id, 3, 256, 128]
             # debug_tool.dump_images_in_batch(images_5d, '/tmp/images_5d/')
             person_ids = sample_batched['person_id']
-            w_h_ratios = sample_batched['w_h_ratios']
+            # w_h_ratios = sample_batched['w_h_ratios']
             actual_size = list(images_5d.size())
             images = images_5d.view([actual_size[0]*sample_size,3,256,128])  # unfolder to 4-D
 
-            if gpu_id >= 0:
-                features, logits = model(Variable(images.cuda(device=gpu_id))) #, Variable(w_h_ratios.cuda(device=gpu_id)))
-                person_ids = person_ids.cuda(device=gpu_id)
+            if len(gpu_ids)>0:
+                person_ids = person_ids.cuda()
+                features, logits = model_p(Variable(images.cuda())) #, Variable(w_h_ratios.cuda(device=gpu_id)))m
             else:
                 features, logits = model(Variable(images)) #model(Variable(images), Variable(w_h_ratios))
             outputs = features.view([actual_size[0], sample_size, -1])
@@ -139,9 +172,10 @@ def main(data_folder, model_folder, sample_size, batch_size,
             #average_meter.update(loss.data.cpu().numpy(), person_ids.cpu().size(0))
             sum_loss+=loss.data.cpu().numpy()
             sum_tri_loss += tri_loss.data.cpu().numpy()
+            time_str = datetime.datetime.now().ctime()
             if i_batch==len(dataloader)-1:
-                log_str = "epoch={0}, iter={1}, train_loss={2}, dist_pos={3}, dist_neg={4} sum_loss_epoch={5}, sum_tri_loss={6}"\
-                    .format(str(epoch), str(i_batch), str(loss.data.cpu().numpy()), str(dist_pos.data.cpu().numpy()),
+                log_str = "time_str: epoch={0}, iter={1}, train_loss={2}, dist_pos={3}, dist_neg={4} sum_loss_epoch={5}, sum_tri_loss={6}"\
+                    .format(time_str, str(epoch), str(i_batch), str(loss.data.cpu().numpy()), str(dist_pos.data.cpu().numpy()),
                             str(dist_neg.data.cpu().numpy()), str(sum_loss), str(sum_tri_loss))
                 print(log_str)
                 if (epoch+1) %(max(1,num_epochs/8))==0:
@@ -156,7 +190,7 @@ if __name__ == '__main__':
     parser.add_argument('model_folder', type=str, help="folder to save the model")
     parser.add_argument('--sample_size', type=int, default=8, help="total number of images of each ID in a sample")
     parser.add_argument('--batch_size', type=int, default=32, help="num samples in a mini-batch, each sample is a sequence of images")
-    parser.add_argument('--gpu_id', type=int, default=0, help="gpu id to use")
+    parser.add_argument('--gpu_ids', type=eval, default=(0,), help="gpu id to use")
     parser.add_argument('--margin', type=float, default=0.1, help="margin for the loss")
     parser.add_argument('--num_epoch', type=int, default=200, help="num of epochs")
     parser.add_argument('--batch_factor', type=float, default=1.5, help="increase batch size by this factor")
@@ -165,6 +199,7 @@ if __name__ == '__main__':
     parser.add_argument('--loss', type=str, default='ranking', help="loss to use")
     parser.add_argument('--lr', type=float, default=0.001, help="learning rate")
     parser.add_argument('--class_th', type=float, default=0.2, help="class threshold")
+    parser.add_argument('--resume', action='store_true', default=False, help="whether to resume from existing ckpt")
     parser.add_argument('--with_roi', action='store_true', default=False, help="whether to use roi")
     parser.add_argument('--original_ar', action='store_true', default=False, help="whether use original aspect ratio")
 
@@ -175,6 +210,5 @@ if __name__ == '__main__':
           format(str(args.sample_size), str(args.batch_size), str(args.margin), str(args.original_ar), str(args.with_roi)))
     torch.backends.cudnn.benchmark = False
     main(args.data_folder, args.model_folder, args.sample_size, args.batch_size,
-         gpu_id=args.gpu_id, margin=args.margin, num_epochs= args.num_epoch, base_model=args.base_model,
-         optimizer_name=args.optimizer, base_lr=args.lr, with_roi=args.with_roi, threshold=args.class_th,
-         original_ar=args.original_ar)
+         num_epochs=args.num_epoch, gpu_ids=args.gpu_ids, margin=args.margin, base_model=args.base_model,
+         optimizer_name=args.optimizer, base_lr=args.lr, with_roi=args.with_roi, threshold=args.class_th)
