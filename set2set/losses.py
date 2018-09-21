d77121664804951d64a6e0eb7376942201b2f1c9
Quan Yuan <yqsony@gmail.com>
2018-09-20 21:58:33 -0700

    triplet loss right version

--- set2set/losses.py
+++ set2set/losses.py
@@ -340,13 +340,43 @@ class WeightedAverageSeqThLoss(nn.Module):
 
         return element_loss[0]+seq_loss[0], element_loss[1], element_loss[2]
 
-class TripletLoss(nn.Module):
+
+class TripletLoss(object):
+  """Modified from Tong Xiao's open-reid (https://github.com/Cysu/open-reid).
+  Related Triplet Loss theory can be found in paper 'In Defense of the Triplet
+  Loss for Person Re-Identification'."""
+  def __init__(self, margin=None):
+    self.margin = margin
+    if margin is not None:
+      self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+    else:
+      self.ranking_loss = nn.SoftMarginLoss()
+
+  def __call__(self, dist_ap, dist_an):
+    """
+    Args:
+      dist_ap: pytorch Variable, distance between anchor and positive sample,
+        shape [N]
+      dist_an: pytorch Variable, distance between anchor and negative sample,
+        shape [N]
+    Returns:
+      loss: pytorch Variable, with shape [1]
+    """
+    y = Variable(dist_an.data.new().resize_as_(dist_an.data).fill_(1))
+    if self.margin is not None:
+      loss = self.ranking_loss(dist_an, dist_ap, y)
+    else:
+      loss = self.ranking_loss(dist_an - dist_ap, y)
+    return loss
+
+
+class TripletLossK(nn.Module):
     """Weighted avearge loss.
        assume the last element of the feature is a weight vector
        """
 
     def __init__(self, margin):
-        super(TripletLoss, self).__init__()
+        super(TripletLossK, self).__init__()
         self.margin = margin
         self.loss_func = RankingLoss(margin=margin)
 
