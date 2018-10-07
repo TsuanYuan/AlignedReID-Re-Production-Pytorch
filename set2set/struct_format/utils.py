fdb1eb730c3a39512d0936b7d9c9174f3a3ec5ad
Quan Yuan <yqsony@gmail.com>
2018-10-07 10:57:03 -0700

    report low quality pids

--- set2set/struct_format/utils.py
+++ set2set/struct_format/utils.py
@@ -248,9 +248,12 @@ class MultiFileCrops(object):
                 i+=1
             except:
                 print "failed to read one image from path {}".format(data_file)
-        if len(images) < count:
-            images = images + low_quality_ones[:count-len(images)]
+        if len(images) < count and len(images) > 0:
+            images = [images[k%len(images)] for k in range(len(images))]
             print 'pid={} has less than {} good quality images'.format(str(pid), str(count))
+        elif len(images) == 0:
+            images = [low_quality_ones[k%len(low_quality_ones)] for k in range(len(low_quality_ones))]
+            print 'pid={} has no good quality images'.format(str(pid), str(count))
         self.pid_pos[pid] = (pos+count)%len(self.pid_index[pid])
         return images
 
